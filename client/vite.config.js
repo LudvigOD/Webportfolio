f5225bb0bb8ad0504aa8779fc026595fb0b7ce80
Ludvig ÖD <ludvig.o.danielsson@gmail.com>
2024-06-25 16:17:52 +0200

    tried fixing fonts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf', '**/*.otf']
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
