2ae4f916af6c2af1e96951187f957c01880602f2
Ludvig ÖD <ludvig.o.danielsson@gmail.com>
2024-11-11 17:16:09 +0100

    Fixed about me

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -5,13 +5,13 @@ function AboutMe() {
     <div className="about-me-section" id="about-me">
       <h2 className="section-title">About Me</h2>
       <p className="about-me-text">
-        Hi there! I'm Ludvig, a dedicated student currently pursuing a degree in Computer Science at Lunds University, Faculty of Engineering. My passion for programming and problem solving drives me to excel in my studies and explore new opportunities in this field.
+        Hi there! I'm Ludvig, a dedicated student on my second year, currently pursuing a degree in Computer Science at Lunds University, Faculty of Engineering.
       </p>
       <p className="about-me-text">
-        At Lunds University, I have taken a diverse range of courses including Scala, Java, Linear Algebra, Calculus, Program Valuation, Photonics, Discrete Structure, and Cognition. These courses have provided me with a solid foundation in both theoretical and practical aspects of programming within diverse areas.
+        At Lunds University, I have taken a diverse range of courses including Scala, Java, Linear Algebra, Multilinear Algebra, Calculus, Program Valuation, Object Oriented Design and Modeling,  Photonics and so on. These courses have provided me with a solid foundation in both theoretical and practical aspects of programming and understanding hardware.
       </p>
       <p className="about-me-text">
-        In my own spare time I have created several projects including: web apps with React and for example MongoDB with Express as the backend, trading bot in python with PyTorch, yahoofinance, pandas, and programming of my own built keyboard.
+        In my own spare time I have created several projects including: web apps with React, trading bot in python with PyTorch, yahoofinance, pandas, and programming of my own built keyboard.
       </p>
       <p className="about-me-text">
         My key skills include solid and deep understanding of programming concepts, testing and valuating programs, and web applications. I have worked on projects such as [Project 1] and [Project 2], where I was able to apply my knowledge and collaborate with peers to achieve our goals.
