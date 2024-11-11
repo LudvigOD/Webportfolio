3cc06b6dbf191a571798e58ec62702ee9a43e2b7
Ludvig ÖD <ludvig.o.danielsson@gmail.com>
2024-11-11 17:52:08 +0100

    Fixed about me

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
     <div className="about-me-section" id="about-me">
       <h2 className="section-title">About Me</h2>
       <p className="about-me-text">
-        Hi there! I'm Ludvig, a dedicated student on my second year, currently pursuing a degree in Computer Science at Lunds University, Faculty of Engineering.
+        Hi there!👋 I'm Ludvig, a dedicated student on my second year, currently pursuing a degree in Computer Science at Lunds University, Faculty of Engineering.
       </p>
       <p className="about-me-text">
         At Lunds University, I have taken a diverse range of courses including Scala, Java, Linear Algebra, Multilinear Algebra, Calculus, Program Valuation, Object Oriented Design and Modeling,  Photonics and so on. These courses have provided me with a solid foundation in both theoretical and practical aspects of programming and understanding hardware.
@@ -14,16 +14,16 @@ function AboutMe() {
         In my own spare time I have created several projects including: web apps with React, trading bot in python with PyTorch, yahoofinance, pandas, and programming of my own built keyboard.
       </p>
       <p className="about-me-text">
-        My key skills include solid and deep understanding of programming concepts, testing and valuating programs, and web applications. I have worked on projects such as [Project 1] and [Project 2], where I was able to apply my knowledge and collaborate with peers to achieve our goals.
+        My key skills include solid and deep understanding of programming concepts, testing and valuating programs, and web applications. Within my studies I have worked on projects such as building an Excel program with an extnesive amount of tools in Java and more, where I was able to apply my knowledge and collaborate with peers to achieve our goals.
       </p>
       <p className="about-me-text">
-        Looking ahead, I aspire to become a valuable peice to a bigger project and learn as much as possible in order for that to happen. I am particularly interested in neuro networks and artificial intelligence and creating software for hardware, and I am eager to continue learning and growing in this field.
+        Looking ahead, I aspire to become a valuable peice to a bigger project and learn as much as possible in order for that to happen. I am particularly interested in neuro networks, artificial intelligence, creating software for hardware, and I am eager to continue learning and growing in this field.
       </p>
       <p className="about-me-text">
-        Outside of my academic pursuits, I enjoy working out, socializing and playing sports. These activities help me stay balanced and inspired.
+        Outside of my academic pursuits, I enjoy working out, socializing and wokring out. These activities help me stay balanced and inspired.
       </p>
       <p className="about-me-text">
-        Feel free to connect with me on [Social Media] or reach out through my <a href="#contact">contact form</a>. I look forward to connecting with you!
+        Feel free to connect with me on ludvig.o.danielsson@gmail.com or reach out through my <a href="#contact">contact form</a>. I look forward to connecting with you!
       </p>
     </div>
   );
